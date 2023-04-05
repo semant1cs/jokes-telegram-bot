23513d6d641cb50f304cb0a91d43a6a965cce980
mihail323i21 <mihail323i21@gmail.com>
2023-04-06 00:48:09 +0500

    newlines at end of files

--- python-bot/keyboard.py
+++ python-bot/keyboard.py
@@ -4,3 +4,4 @@ from telegram import ReplyKeyboardMarkup
 messages_to_handle = ['👍', '👎']
 
 keyboard_markup = ReplyKeyboardMarkup.from_column(messages_to_handle, one_time_keyboard=False, resize_keyboard=True)
+
