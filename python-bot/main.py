d6cb6922eba1bc44764aaf36a21077e7c0d411b9
mihail323i21 <mihail323i21@gmail.com>
2023-04-05 18:27:37 +0500

    мы перешли на python-telegram-bot
    -функция ответа на команду /start
    -функция ответа на реакцию

--- python-bot/main.py
+++ python-bot/main.py
@@ -1,20 +1,31 @@
 import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, MessageHandler, CommandHandler, filters
 
-from aiogram import Bot, Dispatcher, executor, types
 
 API_token = "5878332892:AAF1xsTx7NcmWnKb5xwCMTdcRa2y-qIb4Iw"
 
-logging.basicConfig(level=logging.INFO)
 
-bot = Bot(API_token)
-dispatcher = Dispatcher(bot)
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
 
-@dispatcher.message_handler(commands=['start', 'help'])
-async def send_welcome(message: types.Message):
-    await message.reply(f"Привет, я")
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+async def reply_to_feedback(update, context):
+    await context.bot.send_message(chat_id= update.effective_chat.id, text=f'Ваша оценка: {update.message.text} учтена! Спасибо.')
 
 
 if __name__ == '__main__':
-    executor.start_polling(dispatcher, skip_updates=True)
+    application = ApplicationBuilder().token(API_token).build()
 
+    start_handler = CommandHandler('start', start)
+    feedback_handler = MessageHandler(filters.TEXT, reply_to_feedback)
+
+    application.add_handler(feedback_handler)
+    application.add_handler(start_handler)
+
+    application.run_polling()
