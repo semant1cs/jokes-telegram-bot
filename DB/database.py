ce5a5484fbd2786415748802345cbda5ac8b3dc6
mihail323i21 <mihail323i21@gmail.com>
2023-04-07 23:53:38 +0500

    func clear table database

--- DB/database.py
+++ DB/database.py
@@ -1,13 +1,23 @@
 import sqlite3 as sq
 
+
+def delete_database_table(database_name):
+    cur.execute(f'DROP TABLE {database_name}')
+
+
 with sq.connect("jokes.sqlite") as con:
     cur = con.cursor()
 
-    cur.execute("""CREATE TABLE jokes (
+    cur.execute("""CREATE TABLE IF NOT EXISTS jokes (
     id INTEGER PRIMARY KEY UNIQUE,
     text_field TEXT NOT NULL UNIQUE ,
     likes      INTEGER,
     dislikes   INTEGER
     )""")
+
+    delete_database_table("jokes")
+
+
+
 
 
