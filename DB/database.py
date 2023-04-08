41bb3ac57d75060d52467c8a58015012b99c2566
mihail323i21 <mihail323i21@gmail.com>
2023-04-08 14:14:07 +0500

    mock data + new table for unread jokes

--- DB/database.py
+++ DB/database.py
@@ -1,4 +1,5 @@
 import sqlite3 as sq
+import mock_data
 
 
 def delete_database_table(database_name):
@@ -8,18 +9,23 @@ def delete_database_table(database_name):
 with sq.connect("jokes.sqlite") as con:
     cur = con.cursor()
 
-    delete_database_table('jokes')
 
     cur.execute("""CREATE TABLE IF NOT EXISTS jokes (
-    text_field TEXT NOT NULL UNIQUE ,
+    joke_id INTEGER NOT NULL UNIQUE,
+    text_field TEXT NOT NULL UNIQUE,
     likes      INTEGER NOT NULL DEFAULT 0,
     dislikes   INTEGER NOT NULL DEFAULT 0
     )""")
 
+    cur.execute("""CREATE TABLE IF NOT EXISTS jokes_read(
+    user_id INTEGER NOT NULL,
+    joke_id INTEGER NOT NULL
+    )""")
 
+    query = """INSERT INTO jokes(joke_id, text_field, likes, dislikes)
+     VALUES (?,?,?,?); """
 
+    cur.executemany(query, mock_data.insert_jokes)
 
-
-
-
-
+    con.commit()
+    print(cur.rowcount)
