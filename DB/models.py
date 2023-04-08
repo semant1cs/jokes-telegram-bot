21a1aad990d710985343da2d76962f9a035bd712
mihail323i21 <mihail323i21@gmail.com>
2023-04-08 23:57:17 +0500

    lite create tables

--- DB/models.py
+++ DB/models.py
@@ -1,29 +1,28 @@
 from peewee import *
 
-
 db = SqliteDatabase('jokes.sqlite')
 
-class BaseModel(Model):
-    id = PrimaryKeyField(unique=True)
 
+class BaseModel(Model):
     class Meta:
         database = db
         order_by = 'joke_id'
 
 
 class Joke(BaseModel):
-    joke_id = IntegerField()
+    joke_id = PrimaryKeyField(unique=True)
     text_field = TextField()
     likes = IntegerField()
     dislikes = IntegerField()
     rating = IntegerField()
 
     class Meta:
-        db_table = 'text_field'
+        db_table = 'jokes'
+
 
 class JokeRead(BaseModel):
     joke_id = IntegerField()
     user_id = IntegerField()
 
     class Meta:
-        db_table = 'used_id'
+        db_table = 'jokes_read'
