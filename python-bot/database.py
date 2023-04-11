5930d66be297c4c2cdce78eefa2d8b3103f2abe2
mihail323i21 <mihail323i21@gmail.com>
2023-04-11 14:30:07 +0500

    unread jokes solution

--- python-bot/database.py
+++ python-bot/database.py
@@ -9,13 +9,13 @@ class AddedJoke:
         self.text_joke = text_joke
 
 
-def get_random_joke(count_jokes):
+def get_random_joke_id(count_jokes):
     return random.randint(1, count_jokes)
 
 
 def get_random_joke_from_db():
     with db:
-        id_joke = get_random_joke(len(Joke))
+        id_joke = get_random_joke_id(len(Joke))
 
         joke = Joke.select().where(Joke.joke_id == id_joke)
 
@@ -26,6 +26,29 @@ def get_random_joke_from_db():
 def update_joke_read(joke_id, user_id):
     with db:
         JokeRead.insert(joke_id=joke_id, user_id=user_id).execute()
+
+def get_unread_jokes(user_id):
+    with db:
+        read_jokes = []
+        all_jokes = []
+
+        query_all_jokes = Joke.select()
+        query_read_jokes = JokeRead.select().where(JokeRead.user_id == user_id)
+
+        for joke in query_all_jokes:
+            all_jokes.append(joke.joke_id)
+
+        for joke in query_read_jokes:
+            read_jokes.append(joke.joke_id)
+
+        for joke in read_jokes:
+            if joke in all_jokes:
+                all_jokes.remove(joke)
+
+
+        return all_jokes
+
+print(get_unread_jokes(854998259))
 
 
 # Добавление анекдотов в БД
